23d05d05abb3acda237844640b67a8f483fefc5d
Hai Nguyen <hainv200697@gmail.com>
2024-03-05 01:50:18 +0700

    done

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Web Tools
+# Dev Support
 
 [Docker](#docker) | [Desktop Application](#desktop-application)
 
-[![Web Tools](https://user-images.githubusercontent.com/1680273/171790089-0d1f121a-1940-4d3f-b217-aaf4af4085a7.png)](https://jaywcjlove.github.io/tools)
+[![Dev Support](https://user-images.githubusercontent.com/1680273/171790089-0d1f121a-1940-4d3f-b217-aaf4af4085a7.png)](https://jaywcjlove.github.io/tools)
 
-[![Web Tools](https://user-images.githubusercontent.com/1680273/171790170-25f7c848-6602-4303-9194-f64d26b1c711.png)](https://jaywcjlove.github.io/tools)
+[![Dev Support](https://user-images.githubusercontent.com/1680273/171790170-25f7c848-6602-4303-9194-f64d26b1c711.png)](https://jaywcjlove.github.io/tools)
 
 ## Desktop Application
 
-[![Web Tools for Desktop Application](https://user-images.githubusercontent.com/1680273/175327359-f2e17eb5-26a6-407e-9d45-7af95952c9fb.png)](https://github.com/haitacloveu2/tools/releases)
+[![Dev Support for Desktop Application](https://user-images.githubusercontent.com/1680273/175327359-f2e17eb5-26a6-407e-9d45-7af95952c9fb.png)](https://github.com/haitacloveu2/tools/releases)
 
 ## Docker
 
--- website/src/Router.tsx
+++ website/src/Router.tsx
@@ -71,135 +71,135 @@ export let routes: RouteObject[] = [
     children: [
       {
         index: true,
-        element: <HomePage title="Home - Web Tools" />,
+        element: <HomePage title="Home - Dev Support" />,
       },
       {
         path: 'home',
-        element: <HomePage title="Home - Web Tools" />,
+        element: <HomePage title="Home - Dev Support" />,
       },
       {
         path: 'url-encode',
-        element: <UrlEncode title="URL Encoder/Decoder - Web Tools" />,
+        element: <UrlEncode title="URL Encoder/Decoder - Dev Support" />,
       },
       {
         path: 'json-format',
-        element: <JSONFormat title="JSON Format - Web Tools" />,
+        element: <JSONFormat title="JSON Format - Dev Support" />,
       },
       {
         path: 'js-beautifier',
-        element: <JSBeautifier title="JS Beautifier/Uglify - Web Tools" />,
+        element: <JSBeautifier title="JS Beautifier/Uglify - Dev Support" />,
       },
       {
         path: 'js-obfuscator',
-        element: <JSObfuscator title="JS Obfuscator - Web Tools" />,
+        element: <JSObfuscator title="JS Obfuscator - Dev Support" />,
       },
       {
         path: 'text-case',
-        element: <TextCase title="Text Case - Web Tools" />,
+        element: <TextCase title="Text Case - Dev Support" />,
       },
       {
         path: 'image-to-base64',
-        element: <ImageToBase64 title="Image To Base64 - Web Tools" />,
+        element: <ImageToBase64 title="Image To Base64 - Dev Support" />,
       },
       {
         path: 'code-to-image',
-        element: <CodeToImage title="Code To Image - Web Tools" />,
+        element: <CodeToImage title="Code To Image - Dev Support" />,
       },
       {
         path: 'hash',
-        element: <Hash title="Hash - Web Tools" />,
+        element: <Hash title="Hash - Dev Support" />,
       },
       {
         path: 'generate-password',
-        element: <GeneratePassword title="Generate Password - Web Tools" />,
+        element: <GeneratePassword title="Generate Password - Dev Support" />,
       },
       {
         path: 'html-formatter',
-        element: <HTMLFormatter title="HTML Formatter/Minify - Web Tools" />,
+        element: <HTMLFormatter title="HTML Formatter/Minify - Dev Support" />,
       },
       {
         path: 'html-escape',
-        element: <HTMLEscape title="HTML Escape/Unescape - Web Tools" />,
+        element: <HTMLEscape title="HTML Escape/Unescape - Dev Support" />,
       },
       {
         path: 'html-to-markdown',
-        element: <HtmlToMarkdown title="HTML To Markdown - Web Tools" />,
+        element: <HtmlToMarkdown title="HTML To Markdown - Dev Support" />,
       },
       {
         path: 'markdown-to-html',
-        element: <MarkdownToHtml title="Markdown To HTML - Web Tools" />,
+        element: <MarkdownToHtml title="Markdown To HTML - Dev Support" />,
       },
       {
         path: 'base64-encoder',
-        element: <Base64Encoder title="Base64 Encoder/Decoder Tool - Web Tools" />,
+        element: <Base64Encoder title="Base64 Encoder/Decoder Tool - Dev Support" />,
       },
       {
         path: 'qrcode-generator',
-        element: <QRCodeGenerator title="QRCode Generator Tool - Web Tools" />,
+        element: <QRCodeGenerator title="QRCode Generator Tool - Dev Support" />,
       },
       {
         path: 'chinese-pinyin-conversion',
-        element: <ChinesePinyinConversion title="Chinese Pinyin Conversion Tool - Web Tools" />,
+        element: <ChinesePinyinConversion title="Chinese Pinyin Conversion Tool - Dev Support" />,
       },
       {
         path: 'generate-github-badges',
-        element: <GenerateGithubBadges title="Generate Github Badges Tool - Web Tools" />,
+        element: <GenerateGithubBadges title="Generate Github Badges Tool - Dev Support" />,
       },
       {
         path: 'toml-to-json',
-        element: <TomlToJSON title="Toml To JSON Tool - Web Tools" />,
+        element: <TomlToJSON title="Toml To JSON Tool - Dev Support" />,
       },
       {
         path: 'css-easing-animation',
-        element: <CSSEasingAnimation title="CSS Easing Animation Tool - Web Tools" />,
+        element: <CSSEasingAnimation title="CSS Easing Animation Tool - Dev Support" />,
       },
       {
         path: 'css-formatter',
-        element: <CSSFormatter title="CSS Formatter/Minify Tool - Web Tools" />,
+        element: <CSSFormatter title="CSS Formatter/Minify Tool - Dev Support" />,
       },
       {
         path: 'simple-markdown-editor',
-        element: <SimpleMarkdownEditor title="Simple Markdown Editor Tool - Web Tools" />,
+        element: <SimpleMarkdownEditor title="Simple Markdown Editor Tool - Dev Support" />,
       },
       {
         path: 'jwt-decoder',
-        element: <JWTDecoder title="JWT Decoder Tool - Web Tools" />,
+        element: <JWTDecoder title="JWT Decoder Tool - Dev Support" />,
       },
       {
         path: 'json-yaml',
-        element: <JSON2Yaml title="JSON/YAML Converter Tool - Web Tools" />,
+        element: <JSON2Yaml title="JSON/YAML Converter Tool - Dev Support" />,
       },
       {
         path: 'json-xml',
-        element: <JSON2XML title="JSON/XML Converter Tool - Web Tools" />,
+        element: <JSON2XML title="JSON/XML Converter Tool - Dev Support" />,
       },
       {
         path: 'json-ini',
-        element: <JSON2INI title="JSON/ini Converter Tool - Web Tools" />,
+        element: <JSON2INI title="JSON/ini Converter Tool - Dev Support" />,
       },
       {
         path: 'pdf-to-img',
-        element: <PDFToImg title="PDF To Image Tool - Web Tools" />,
+        element: <PDFToImg title="PDF To Image Tool - Dev Support" />,
       },
       {
         path: 'exif-viewer',
-        element: <ExifViewer title="Exif Viewer Tool - Web Tools" />,
+        element: <ExifViewer title="Exif Viewer Tool - Dev Support" />,
       },
       {
         path: 'svg-optimizer',
-        element: <SVGOptimizer title="SVG Optimizer Tool - Web Tools" />,
+        element: <SVGOptimizer title="SVG Optimizer Tool - Dev Support" />,
       },
       {
         path: 'amount-in-rmb',
-        element: <AmountInRMB title="Amount In RMB Tool - Web Tools" />,
+        element: <AmountInRMB title="Amount In RMB Tool - Dev Support" />,
       },
       {
         path: 'standard-chinese-characters',
-        element: <StandardChineseCharactersRMB title="Table of General Standard Chinese Characters - Web Tools" />,
+        element: <StandardChineseCharactersRMB title="Table of General Standard Chinese Characters - Dev Support" />,
       },
       {
         path: 'simplified-traditional-chinese',
-        element: <SimplifiedTraditionalChinese title="Simplified Traditional Chinese Converter - Web Tools" />,
+        element: <SimplifiedTraditionalChinese title="Simplified Traditional Chinese Converter - Dev Support" />,
       },
       { path: '*', element: <NoMatch /> },
     ],
